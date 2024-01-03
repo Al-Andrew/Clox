42e74c7a498968426d5a234af70356e8f4dffe31
Andrei ALDEA <aldea.andrei.977@gmail.com>
2024-01-03 22:05:58 +0200

    common: add fast-path for interned strings comparison

--- src/common.c
+++ src/common.c
@@ -2,6 +2,10 @@
 
 
 int s8_compare(s8 s1, s8 s2) {
+    if(s1.len == s2.len && s1.string == s2.string) {
+        return 0;
+    }
+
     int idx = 0;
     int min_idx = s1.len > s2.len?s2.len:s1.len;
     while(idx < min_idx) {
