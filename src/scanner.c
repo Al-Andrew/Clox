0387ee35b522dc4f5587f61bc7acbf08015d9ecb
Andrei ALDEA <aldea.andrei.977@gmail.com>
2024-01-03 14:24:14 +0200

    scanner: fix keyword scanning

--- src/scanner.c
+++ src/scanner.c
@@ -133,16 +133,17 @@ typedef struct {
 #define s8d$(str) (s8){.string = str, .len = strlen(str)}
 
 int s8_compare(s8 s1, s8 s2) {
-    if(s1.len != s2.len) return false;
-
     int idx = 0;
-    while(idx < s1.len) {
+    int min_idx = s1.len > s2.len?s2.len:s1.len;
+    while(idx < min_idx) {
         int diff = s1.string[idx] - s2.string[idx];
         if( diff != 0 ) {
             return diff;
         }
+        ++idx;
     }
-    return 0;
+
+    return s1.len - s2.len;
 }
 
 static const s8 s_clox_keywords[] = {
