e603b6ab2c3fc33534577404f2f79ef47e807849
Andrei ALDEA <aldea.andrei.977@gmail.com>
2024-01-05 15:16:06 +0200

    xmake: remove wpedantic

--- xmake.lua
+++ xmake.lua
@@ -4,7 +4,7 @@ set_description("My implemetation of clox. see https://craftinginterpreters.com/
 set_version("0.0.1")
 set_xmakever("2.8.5")
 
-set_warnings("all", "error", "pedantic")
+set_warnings("all", "error")
 set_languages("c11")
 
 add_rules("mode.debug", "mode.release")
